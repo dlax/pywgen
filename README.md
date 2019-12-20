b32902b10192592005824f7547e6de1d90ed74cf
Denis Laxalde <denis@laxalde.org>
2019-12-20 16:20:01 +0100

    Initialize the "pywgen" project
    No code for now, just a README, a LICENSE file and a setup.py containing
    minimal metadata. We require Python >= 3.6 because we'd probably use the
    "secrets" module from the standard library. We use setuptools_scm to
    manage versioning from VCS metadata.
    
    Finally, we include a tox configuration with a "black" and a
    "check-manifest" environments; the latter makes sure the code follows
    standard formatting/style practices, the latter makes sure the source
    distribution (sdist) contains everything needed (as compared with VCS
    content).

---
+++ README.md
@@ -0,0 +1,9 @@
+# pywgen - generate pronounceable passwords
+
+pywgen is a Python program to generate passwords which are designed to be
+easily memorized by humans, while being as secure as possible.
+
+## Running tests
+
+Tests can be run using [tox](https://tox.readthedocs.io/) by simply executing
+the `tox` in a terminal.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pywgen",
+    author="Denis Laxalde",
+    author_email="denis@laxalde.org",
+    description="generate pronounceable passwords",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.sr.ht/~dlax/pywgen",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+)
