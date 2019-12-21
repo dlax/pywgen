26848bd0c586b035398933c4efc57c76cb38440a
Denis Laxalde <denis@laxalde.org>
2019-12-21 11:33:53 +0100

    Add a "capitalize" option to generate_password()
    This be used to handle -A/--no-capitalize and -c/--capitalize
    options of pwgen.

--- pywgen.py
+++ pywgen.py
@@ -4,16 +4,32 @@ import argparse
 import pkg_resources
 import secrets
 import string
+from typing import List, Optional
 
 
-def generate_password(length: int, numerals: bool = True) -> str:
+def has_capitals(values: List[str]) -> bool:
+    return any(v.isupper() for v in values)
+
+
+def generate_password(
+    length: int, numerals: bool = True, capitalize: Optional[bool] = None
+) -> str:
     """Return one password of specified `length` possibly excluding/including
     character types matching keyword arguments.
+
+    `capitalize` controls whether produced password must (True value), may
+    (None value) or must not (False value) contain capital letters.
     """
-    chars = string.ascii_letters
+    if capitalize is False:
+        chars = string.ascii_lowercase
+    else:
+        chars = string.ascii_letters
     if numerals:
         chars += string.digits
-    return "".join(secrets.choice(chars) for _ in range(length))
+    while True:
+        elements = [secrets.choice(chars) for _ in range(length)]
+        if not capitalize or (capitalize and has_capitals(elements)):
+            return "".join(elements)
 
 
 def get_parser() -> argparse.ArgumentParser:
--- tests/test_pywgen.py
+++ tests/test_pywgen.py
@@ -3,12 +3,24 @@ from unittest.mock import patch
 
 import pytest
 
-from pywgen import generate_password
+from pywgen import generate_password, has_capitals
+
+
+@pytest.mark.parametrize(["value", "result"], [("Ab6", True), ("123rt", False)])
+def test_has_capitals(value, result):
+    values = list(value)
+    assert has_capitals(values) == result
 
 
 @pytest.mark.parametrize(
     ["length", "options", "pattern"],
-    [(8, {}, r"[a-zA-Z0-9]{8}"), (2, {"numerals": False}, r"[a-zA-Z]{2}")],
+    [
+        (8, {}, r"[a-zA-Z0-9]{8}"),
+        (2, {"numerals": False}, r"[a-zA-Z]{2}"),
+        (7, {"capitalize": None}, r"[A-Za-z0-9]{7}"),
+        (5, {"capitalize": False}, r"[a-z0-9]{5}"),
+        (10, {"capitalize": True}, r"[A-Z]+[a-z0-9]+|[a-z0-9]+[A-Z]+"),
+    ],
 )
 def test_generate_password_produces_expected_characters(length, options, pattern):
     pw = generate_password(length, **options)
